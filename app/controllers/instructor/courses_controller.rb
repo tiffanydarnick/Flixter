6b6e8d4362897e78d95aa1f6290236300356f61e
tiffanydarnick <tdarnick@outlook.com>
2015-11-07 22:06:04 +0000

    permission

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,7 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, :only => [:show]
+
 	def new
 		@course=Course.new
 	end
@@ -13,13 +15,23 @@ class Instructor::CoursesController < ApplicationController
       end  	
   end
 
-  	def show
-  		@course = Course.find(params[:id])
-  	end
+  def show
+  end
 
   private
 
-  	def course_params
-   		params.require(:course).permit(:title, :description, :cost)
-  	end
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render :text => "Unauthorized", :status => :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+   	params.require(:course).permit(:title, :description, :cost)
+  end
 end
