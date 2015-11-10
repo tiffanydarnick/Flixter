7e87ceb001ad730fe2f8e5befc6e9c344c5bc815
tiffanydarnick <tdarnick@outlook.com>
2015-11-10 01:33:52 +0000

    enroll verify

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_course_enrollment_for_lesson, :only => :show
+	
 	def show
 	end
 
 	private
+
+	def require_course_enrollment_for_lesson
+		active_course = current_lesson.section.course
+		 if current_lesson.section.course.user.enrolled_in?(active_course) 
+		 	redirect_to course_path(active_course), alert: 'Please enroll to access lesson'
+		 end
+    end
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
